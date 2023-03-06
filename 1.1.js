3536503a7f3f7cc27faa9f6d7bb4c46475af489e
Lexandrina90 <lexandrina90@gmail.com>
2023-03-06 17:06:25 +0300

    First 2 tasks refactoring

--- 1.1.js
+++ 1.1.js
@@ -1,10 +1,29 @@
+function deepClone(obj) {
+    if (typeof obj !== 'object') {
+      return obj;
+    }
+    let newObj = null;
+    if (Array.isArray(obj)) {
+      newObj = [...obj]
+    } else {
+       newObj = {...obj}
+    }
+    for(key in newObj) {
+       newObj[key] = deepClone(newObj[key])
+    }
+  return newObj;
+}
+
+/*
 function cloneDeep(obj) {   
     if (obj === null) return null;
     let clone = Object.assign({}, obj);
-    Object.keys(clone).forEach(key => (clone[key] = typeof obj[key] ==='object'  ? cloneDeep(obj[key]) : obj[key]));
+    Object.keys(clone).forEach(key => 
+        (clone[key] = typeof obj[key] ==='object'  ? cloneDeep(obj[key]) : obj[key]));
     if (Array.isArray(obj)) {
         clone.length = obj.length;
         return Array.from(clone);
     } 
     return clone;
     }
+*/
--- 1.2.js
+++ 1.2.js
@@ -1,6 +1,20 @@
+
+function compare (arr) {
+    let newArr = arr.map((el,i,a,) => 
+        a[i+1] && el[0] === a[i+1][0] && el.slice(-1) === a[i+1].slice(-1));
+    let res = newArr.slice(0,-1);
+    return res;
+}
+
+
+console.log(compare(["asd", "afffd", "cc", "kk"]));
+
+/* 
 function compare (arr) {
     return arr.map((el,i,a) => a[i+1] && el[0] === a[i+1][0] && el.slice(-1) === a[i+1].slice(-1)).slice(0,-1);
 }
 
 
 console.log(compare(["asd", "afffd", "cc", "kk"]));
+*/
+
